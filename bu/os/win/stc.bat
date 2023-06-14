29e3c67ad74dfd104574221d0295aff1e265ecc4
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2023-06-14 23:00:41 +0530

    eeee

--- bu/os/win/stc.bat
+++ bu/os/win/stc.bat
@@ -1,4 +1,6 @@
-doskey tcpg=curl http://localhost:8181/customersPG
-doskey tcflask=curl http://localhost:8181/customersFlask
-doskey tcredis=curl http://localhost:8181/customersRedis/pino
-doskey tckafka=curl -d "{\"id\": 9191,\"name\": \"Curlo\"}" -H "Content-Type: application/json" -X POST http://localhost:8181/customersKafka
+doskey tcst=gradle clean bootTestRun
+
+doskey 1=curl http://localhost:8181/customersPG
+doskey 2=curl http://localhost:8181/customersFlask
+doskey 3=curl http://localhost:8181/customersRedis/pino
+doskey 4=curl -d "{\"id\": 9191,\"name\": \"Curlo\"}" -H "Content-Type: application/json" -X POST http://localhost:8181/customersKafka
